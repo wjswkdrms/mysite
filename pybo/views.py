077c7054877c0cf001d5932349f634e921a6eb9f
wjswkdrms <wjswkdrms7@gmail.com>
2025-06-04 17:50:52 +0900

    1212

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-from django.http import HttpResponse
-
-# Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
